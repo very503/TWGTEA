4a314b6d65ad385b905cbb8719a6a8d259b03a48
very503 <sarasa53@naver.com>
2022-07-20 14:09:22 +0900

    0720

--- js/twg_index.js
+++ js/twg_index.js
@@ -1,24 +1,73 @@
 $(function(){
     var wd = $(window).width();
+    var i = 0;
+    var ind = 0;
     var mainG = $('.mnGroup');
-    var auto = setInterval(leftslide,1000);
+    var auto = setInterval(rightSlide,3000);
 
-    function leftslide(){
-        i++;
-        i=i%3;
-        mainG.css({ marginLeft : i*-100+'%'});
+    mainG.find('.mn:last').prependTo(mainG);
+    mainG.css({ marginLeft : '-100%' });
+    $('.control>.btns>li:eq(0)').click(leftSlide);
+    $('.control>.btns>li:eq(1)').click(rightSlide);
+    $('.control>.btns>li:eq(2)').click(stopSlide);
+    
+
+
+    function leftSlide(){
+        //무한루프
+        mainG.stop().animate({
+            marginLeft : '0%'
+        },2000,function(){
+            mainG.find('.mn:last').prependTo(mainG);
+            mainG.css({marginLeft : '-100%'});
+        });
+
+        ind =  mainG.find('.mn:eq(0)').attr('data-num');
+
+         //바
+         bar4.find('.bar .barbg').css({
+            width : ind*33.33+'%',
+            height : '5px',
+            backgroundColor : '#333',
+            transition : 'all 0.5s'
+        });
+        if(ind == 3){
+            $('.cnt01 .bar').find('.bar .barbg').css({display:'none'});
+            setTimeout(function(){
+                $('.cnt01 .bar').find('.bar .barbg').css({display:'block'})
+            },200);
+        };
     };
-    function rightslide(){
-        i++;
-        i=(3+i)%3;
-        mainG.css({ marginLeft : i*-100+'%'});
+    function rightSlide(){
+        mainG.stop().animate({
+            marginLeft : '-200%'
+        },2000,function(){
+            mainG.find('.mn:first').appendTo(mainG);
+            mainG.css({marginLeft : '-100%'});
+        });
+
+        ind =  mainG.find('.mn:eq(2)').attr('data-num');
+
+         //바
+         bar4.find('.bar .barbg').css({
+            width : ind*33.33+'%',
+            height : '5px',
+            backgroundColor : '#333',
+            transition : 'all 0.5s'
+        });
+        if(ind == 3){
+            $('.cnt01 .bar').find('.bar .barbg').css({display:'none'});
+            setTimeout(function(){
+                $('.cnt01 .bar').find('.bar .barbg').css({display:'block'})
+            },200);
+        };
     };
     function stopSlide(){
         clearInterval(auto);
     };
-    function playSlide(){
-        auto = setInterval(leftslide,1000);
-    };
+   
+
+    
 
 
     if(wd > 1024){
